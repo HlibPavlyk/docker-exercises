3222a522074b03a2b6d4f946bf89cad416d6d4f3
HlibPavlyk <pavlikgo78@gmail.com>
2024-05-24 15:25:12 +0300

    feat: complete program
    - that program generate random hacker sentence
    - also it use command line args

---
+++ dotnet/CommandLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet
+{
+    public class CommandLineOptions
+    {
+        [Option('n', "name", Required = true, HelpText = "Enter your name")]
+        public string? Name { get; set; }
+    }
+}
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bogus;
+using CommandLine;
+using dotnet;
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Parser.Default.ParseArguments<CommandLineOptions>(args)
+    .WithParsed(options =>
+    {
+        var faker = new Faker();
+
+        string responsePhrase = faker.Hacker.Phrase();
+        string responseName = faker.Name.FullName();
+
+        Console.WriteLine($"Hi, {options.Name}. I'm {responseName}. {responsePhrase}");
+
+    });
